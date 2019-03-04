b100f044a348e5e799debd6aeca7bdbd8c7811df
Guillermo Harosteguy <harosteguy@gmail.com>
2019-03-03 18:42:51 -0600

    Se quita la API Notificaciones de la rama master

--- db/usuarios.sql
+++ db/usuarios.sql
@@ -3,7 +3,6 @@ CREATE USER 'cms_usr'@'localhost' IDENTIFIED WITH mysql_native_password AS '***'
 GRANT USAGE ON *.* TO 'cms_usr'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_articulus`.* TO 'cms_usr'@'localhost';
 GRANT SELECT ON `cms\_contenidos`.* TO 'cms_usr'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_notificaciones`.* TO 'cms_usr'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_usuarios`.* TO 'cms_usr'@'localhost';
 
 CREATE USER 'cms_wm'@'localhost' IDENTIFIED WITH mysql_native_password AS '***';
@@ -11,5 +10,4 @@ CREATE USER 'cms_wm'@'localhost' IDENTIFIED WITH mysql_native_password AS '***';
 GRANT USAGE ON *.* TO 'cms_wm'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_contenidos`.* TO 'cms_wm'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_articulus`.* TO 'cms_wm'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `cms\_notificaciones`.* TO 'cms_wm'@'localhost';
 GRANT SELECT, INSERT, UPDATE ON `cms\_usuarios`.* TO 'cms_wm'@'localhost';
